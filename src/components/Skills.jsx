035172fb9692a1fa86d7ad2ab6ae6c373dbc9ee6
Sourjya-4187 <sourjya.pradhan@gmail.com>
2023-12-22 21:44:03 +0530

    Skill Container Done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import { LinearProgress, ThemeProvider, Typography, createTheme } from "@mui/material";
+import { ThemeProvider, Typography, createTheme } from "@mui/material";
 import React from "react";
 
 let Bg = require('../images/Nbg.jpg');
@@ -7,6 +7,28 @@ const theme = createTheme({
     typography: {
       fontFamily:'Oswald'
     },});
+
+const Styles = {
+    skillContainer:{
+        minWidth:'300px',
+        margin:'0 auto',
+        padding:'0 10px'
+    },
+    marginForBar:{
+        margin:'5px 0 3px'
+    },
+    fontAndPadding:{
+        fontSize:'1.4em', 
+        padding:'20px'
+    },
+    textAlignInLine:{
+        textAlign:'left'
+    },
+    boxStyling:{
+        position:'relative',
+        boxShadow:'0 -4px 8px -5px #333'
+    }
+}
 
 const technologyProgressBarvalue = [
     {
@@ -39,126 +61,110 @@ const technologyProgressBarvalue = [
 const projectProgressBar = [
     {
         name:'Jira',
-        value:'95'
+        value:95
     },
     {
         name:'Bit bucket',
-        value:'91'
+        value:91
     },
     {
         name:'GIT',
-        value:'95'
+        value:95
     },
     {
         name:'Agile Methodologies',
-        value:'90'
+        value:90
     }
-]
+];
+
+const ProgressBarComp = ({props}) => {
+    return(
+        <div className="progress">
+            <div className="progress-value" style={{ '--my-var': `${props}%`}}></div>
+        </div>
+    ) 
+};
 
 const Skills = () =>{
     return(
         <ThemeProvider theme={theme}>
             <div className="topic-container middle-container-image" style={{ backgroundImage:{Bg}}}>
                 <div style={{marginBottom:'40px'}} id='skills'>
-                    <div style={{position:'relative',boxShadow:'0 -4px 8px -5px #333'}}>
+                    <div style={Styles.boxStyling}>
                         <div style={{padding:'0 0 30px'}}>
                             <div style={{fontSize:'3em',padding:'10px',color:'white'}}>Skills</div>
                             <div style={{ display:'flex', flexDirection:'row',flexWrap:'wrap',marginBottom:'20px',gap:'15px' }}>
                                 {/* SKILL CONTAINER */}
-                                <div style={{minWidth:'300px',margin:'0 auto',padding:'0 10px'}} id='Web Technologies'>
-                                    <Typography className="body" sx={{ fontSize:'1.4em', padding:'20px' }}color='white'>Web Technologies</Typography>
+                                <div style={Styles.skillContainer} id='Web Technologies'>
+                                    <Typography className="body" sx={Styles.fontAndPadding}color='white'>Web Technologies</Typography>
                                     <div>
                                         {
                                             technologyProgressBarvalue.map((item)=>(
                                                     <>
-                                                    <Typography color='white'>{item.name}</Typography>
-                                                    <LinearProgress
-                                                        color="success"
-                                                        value={item.value}
-                                                        variant='determinate'
-                                                        sx={{
-                                                            animationDuration: "43s"
-                                                          }}
-                                                    />
+                                                    <Typography color='white' sx={Styles.textAlignInLine}>{item.name}</Typography>
+                                                    <div style={Styles.marginForBar}>
+                                                        <ProgressBarComp props={item.value} />
+                                                    </div>
                                                 </>                                             
                                             ))
                                         }
-                                        
                                     </div>
                                 </div>
 
-                                <div style={{minWidth:'300px',margin:'0 auto',padding:'0 10px'}} id='Libraries and Software'>
-                                    <Typography sx={{ fontSize:'1.4em', padding:'20px' }}color='white'>Libraries and Software</Typography>
-                                    <Typography color='white'>React Router</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={91}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Redux</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={81}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Material-UI</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={95}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Jest</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={90}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Formik</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={40}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Figma</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={61}
-                                        variant='determinate'
-                                    />
+                                <div style={Styles.skillContainer} id='Libraries and Software'>
+                                    <Typography sx={Styles.fontAndPadding}color='white'>Libraries and Software</Typography>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>React Router</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={91} />
+                                            </div>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Redux</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={81} />
+                                            </div>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Material-UI</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={95} />
+                                            </div>    
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Jest</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={90} />
+                                            </div> 
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Formik</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={40} />
+                                            </div>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Figma</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={61} />
+                                            </div>
                                 </div>
 
-                                <div style={{minWidth:'300px',margin:'0 auto',padding:'0 10px'}} id='Operations'>
-                                    <Typography sx={{ fontSize:'1.4em', padding:'20px' }}color='white'>Operations</Typography>
+                                <div style={Styles.skillContainer} id='Operations'>
+                                    <Typography sx={Styles.fontAndPadding}color='white'>Operations</Typography>
                                         <div>
                                             {
                                                 projectProgressBar.map((item)=>(
                                                     <>
-                                                        <Typography color='white'>{item.name}</Typography>
-                                                        <LinearProgress
-                                                            color="success"
-                                                            value={item.value}
-                                                            variant='determinate'
-                                                        />
+                                                        <Typography color='white' sx={Styles.textAlignInLine}>{item.name}</Typography>
+                                                            <div style={Styles.marginForBar}> 
+                                                                <ProgressBarComp props={item.value} />
+                                                            </div>
                                                     </>
                                                 ))
                                             }
                                         </div>
                                 </div>
 
-                                <div style={{minWidth:'300px',margin:'0 auto',padding:'0 10px'}} id='JS Bundlers'>
-                                    <Typography sx={{ fontSize:'1.4em', padding:'20px' }}color='white'>JS Bundlers</Typography>
-                                    <Typography color='white'>Webpack</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={90}
-                                        variant='determinate'
-                                    />
-                                    <Typography color='white'>Vite</Typography>
-                                    <LinearProgress
-                                        color="primary"
-                                        value={90}
-                                        variant='determinate'
-                                    />
+                                <div style={Styles.skillContainer} id='JS Bundlers'>
+                                    <Typography sx={Styles.fontAndPadding}color='white'>JS Bundlers</Typography>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Webpack</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={91} />
+                                            </div>
+                                        <Typography color='white' sx={Styles.textAlignInLine}>Vite</Typography>
+                                            <div style={Styles.marginForBar}> 
+                                                <ProgressBarComp props={90} />
+                                            </div>
                                 </div>
 
                             </div>
